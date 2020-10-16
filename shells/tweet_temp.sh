e3af9ff75fd246c57df20ce34b2280b22d38fb63
zeriyoshi <zeriyoshi@gmail.com>
2020-10-17 00:15:24 +0900

    tweet_temp: fix: LN

--- shells/tweet_temp.sh
+++ shells/tweet_temp.sh
@@ -4,9 +4,11 @@ TWEET_BIN="${SCRIPT_DIR}/../kotoriotoko/BIN/tweet.sh"
 
 CPU_TEMP="$(cat "/sys/class/thermal/thermal_zone0/temp" | sed -e 's/^\(.\{2\}\)/\1./') ℃"
 HDD_TEMPS=""
+LN="
+"
 
-for DEVICE in $(find "/dev" -name "sd*");do
-    HDD_TEMPS="$(printf "${HDD_TEMPS}${DEVICE}: $(/usr/sbin/hddtemp -n "${DEVICE}") ℃\n")"
+for DEVICE in $(find "/dev" -name "sd*"); do
+    HDD_TEMPS="${HDD_TEMPS}${DEVICE}: $(/usr/sbin/hddtemp -n "${DEVICE}") ℃${LN}"
 done
 
 printf "CPU: ${CPU_TEMP}\nHDDs:\n${HDD_TEMPS}\n$(date +"%Y-%m-%d %H:%M:%S")" | "${TWEET_BIN}"
