dadf288bac506e71abce41443c70075e35ee9d85
zeriyoshi <zeriyoshi@gmail.com>
2020-10-16 22:45:09 +0900

    any: add date

--- shells/btrfs_maintenance.sh
+++ shells/btrfs_maintenance.sh
@@ -5,13 +5,13 @@ TWEET_AT="@zeriyoshi"
 TARGET_MOUNT_POINT="/storage"
 
 if [ ! -d "${TARGET_MOUNT_POINT}" ]; then
-    "${TWEET_BIN}" "[btrfs_maintenance] ${TARGET_MOUNT_POINT} not found. ${TWEET_AT}"
+    "${TWEET_BIN}" "[btrfs_maintenance] ${TARGET_MOUNT_POINT} not found. $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
     exit 2
 fi 
 
-"${TWEET_BIN}" "[btrfs_maintenance] starting defrag... ${TWEET_AT}"
+"${TWEET_BIN}" "[btrfs_maintenance] starting defrag... $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
 btrfs filesystem defragment -r -v "${TARGET_MOUNT_POINT}"
-"${TWEET_BIN}" "[btrfs_maintenance] complete defrag. ${TWEET_AT}"
+"${TWEET_BIN}" "[btrfs_maintenance] complete defrag. $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
 
 sleep 10
 
--- shells/junk_file_clean.sh
+++ shells/junk_file_clean.sh
@@ -12,4 +12,4 @@ fi
 
 find "${TARGET_MOUNT_POINT}" \( -name '.DS_Store' -o -name '._*' -o -name '.apdisk' -o -name 'Thumbs.db' -o -name 'Desktop.ini' \) -delete -print > "${LOG_PATH}"
 
-"${TWEET_BIN}" "[junk_file_clean] ${COUNT} files deleted. (log: /tmp/junk_file_clean_list) ${TWEET_AT}"
+"${TWEET_BIN}" "[junk_file_clean] ${COUNT} files deleted. (log: /tmp/junk_file_clean_list) $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
