aef386c2c954ce03ab12b5238ba11412d3c8df67
zeriyoshi <zeriyoshi@gmail.com>
2020-10-22 21:18:52 +0900

    image_backup: add date

--- shells/image_backup.sh
+++ shells/image_backup.sh
@@ -7,14 +7,14 @@ TARGET_DIR="/nas_image_backup"
 TARGET_DEV="/dev/mmcblk1"
 
 if [ ! -d "${TARGET_MOUNT_POINT}" ]; then
-    "${TWEET_BIN}" "[image_backup] ${TARGET_MOUNT_POINT} missing. ${TWEET_AT}"
+    "${TWEET_BIN}" "[image_backup] ${TARGET_MOUNT_POINT} missing. $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
 fi
 
 if [ ! -d "${TARGET_MOUNT_POINT}${TARGET_DIR}" ]; then
-    "${TWEET_BIN}" "[image_backup] ${TARGET_MOUNT_POINT}${TARGET_DIR} missing. ${TWEET_AT}"
+    "${TWEET_BIN}" "[image_backup] ${TARGET_MOUNT_POINT}${TARGET_DIR} missing. $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
 fi
 
 OUT_PATH="${TARGET_MOUNT_POINT}${TARGET_DIR}/$(date +"%Y%m%d").img.gz"
-"${TWEET_BIN}" "[image_backup] image backup started: ${OUT_PATH} ${TWEET_AT}"
+"${TWEET_BIN}" "[image_backup] image backup started: ${OUT_PATH} $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
 dd if="${TARGET_DEV}" bs=1M | gzip -c > "${OUT_PATH}"
-"${TWEET_BIN}" "[image_backup] image backup finished: ${OUT_PATH} ${TWEET_AT}"
+"${TWEET_BIN}" "[image_backup] image backup finished: ${OUT_PATH} $(date +"%Y-%m-%d %H:%M:%S") ${TWEET_AT}"
